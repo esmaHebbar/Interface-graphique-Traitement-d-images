9704133dbf2fc9ec0a3eefaeaeccb7b1d26c69dd
unknown <ehebbar001@ensc.fr>
2024-06-07 09:54:01 +0200

    merge de la branche guigui

---
+++ model_HH.py
@@ -0,0 +1,109 @@
+from brian2 import *
+import matplotlib.pyplot as plt
+
+# Définir la taille du pas de temps pour le solveur d'équations différentielles
+defaultclock.dt = 0.01*ms
+
+# Paramètres du modèle de Hodgkin-Huxley
+Cm = 1.0*ufarad
+El = 10.6*mV
+ENa = 120.0*mV
+EK = -12.0*mV
+gl0 = 0.3*msiemens
+gNa0 = 120.0*msiemens
+gK0 = 36.0*msiemens
+
+# Équations différentielles pour le modèle de Hodgkin-Huxley
+eqs = '''
+dv/dt = (I - gl * (v-El) - gNa * m**3 * h * (v-ENa) - gK * n**4 * (v-EK))/Cm : volt
+I : amp (constant)  # Courant appliqué
+
+dn/dt = alphan * (1-n) - betan * n : 1
+dm/dt = alpham * (1-m) - betam * m : 1
+dh/dt = alphah * (1-h) - betah * h : 1
+
+alphan = (0.01/mV) * 10*mV/exprel((-v+10*mV)/(10*mV))/ms : Hz
+betan = 0.125*exp(-v/(80*mV))/ms : Hz
+
+alpham = (0.1/mV) * 10*mV/exprel((-v+25*mV)/(10*mV))/ms : Hz
+betam = 4 * exp(-v/(18*mV))/ms : Hz
+
+alphah = 0.07 * exp(-v/(20*mV))/ms : Hz
+betah = 1/(exp((-v+30*mV) / (10*mV)) + 1)/ms : Hz
+
+gNa : siemens
+gK : siemens
+gl : siemens
+'''
+
+# Créer un groupe de neurones avec les équations définies
+HH = NeuronGroup(1, eqs, threshold='v>-20*mV', reset='v=El', refractory=5*ms,method='rk4')
+
+# Initialisation des variables
+HH.v = El
+HH.h = 0.75
+HH.m = 0.15
+HH.n = 0.35
+HH.gNa = gNa0
+HH.gK = gK0
+HH.gl = gl0
+
+# Enregistrer les variables d'état et les spikes
+statemon = StateMonitor(HH, True, record=True)
+spikemon = SpikeMonitor(HH)
+
+# Simulation avec différents courants appliqués
+HH.I = 0.0*uA
+run(50*ms, report='text')
+HH.I = 60.0*uA
+run(50*ms, report='text')
+HH.I = 0.0*uA
+run(50*ms, report='text')
+
+# Traçage des résultats
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(statemon.t/ms, statemon.v[0])
+plt.xlabel('Time (ms)')
+plt.ylabel('V')
+
+plt.subplot(2, 1, 2)
+plt.plot(statemon.t/ms, statemon.I[0]/uA, label='Applied Current')
+plt.xlabel('Time (ms)')
+plt.ylabel('Current (uA)')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.plot(spikemon.t/ms, spikemon.i, '.k')
+plt.xlabel('Time (ms)')
+plt.ylabel('Neuron index')
+plt.show()
+
+
+# figure()
+# plot(statemon.t/ms, statemon.n[0], label='n')
+# plot(statemon.t/ms, statemon.m[0], label='m')
+# plot(statemon.t/ms, statemon.h[0], label='h')
+# xlabel('Time (ms)')
+# ylabel('Probabilty')
+# legend()
+# show()
+
+# figure()
+# plot(statemon.t/ms, statemon.gNa[0]*(statemon.m[0]**3)*statemon.h[0], label='Na')
+# plot(statemon.t/ms, statemon.gK[0]*(statemon.n[0]**4), label='K')
+# xlabel('Time (ms)')
+# ylabel('Conductances (mS)')
+# legend()
+# show()
+
+# figure()
+# plot(statemon.t/ms, statemon.gNa[0]*(statemon.m[0]**3)*statemon.h[0]*(statemon.v[0]-ENa), label='I_Na')
+# plot(statemon.t/ms, statemon.gK[0]*(statemon.n[0]**4)*(statemon.v[0]-EK), label='I_K')
+# plot(statemon.t/ms, statemon.gl[0]*(statemon.v[0]-El) + statemon.gK[0]*(statemon.n[0]**4)*(statemon.v[0]-EK) +
+#      statemon.gNa[0]*(statemon.m[0]**3)*statemon.h[0]*(statemon.v[0]-ENa), label='I_Na + I_K + I_l')
+# xlabel('Time (ms)')
+# ylabel('Currents (uA)')
+# legend()
+# show()
--- neurone_avec_spikes.py
+++ neurone_avec_spikes.py
@@ -33,11 +33,7 @@ tau = 20*ms
 delta = 2*ms
 taurefr = 2*ms
 duration = .1*second
-C = 1000
-sparseness = float(C)/N
-J = .1*mV
-muext = 25*mV
-sigmaext = 1*mV
+
 
 # Fonction de simulation neuronale
 def run_neuron_simulation():
@@ -64,6 +60,67 @@ def run_neuron_simulation():
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# Paramètres du modèle de Hodgkin-Huxley
+Cm = 1.0*ufarad
+El = 10.6*mV
+ENa = 120.0*mV
+EK = -12.0*mV
+gl0 = 0.3*msiemens
+gNa0 = 120.0*msiemens
+gK0 = 36.0*msiemens
+
+defaultclock.dt = 0.01*ms
+
+# Équations différentielles pour le modèle de Hodgkin-Huxley
+eqs = '''
+dv/dt = (I - gl * (v-El) - gNa * m**3 * h * (v-ENa) - gK * n**4 * (v-EK))/Cm : volt
+I : amp (constant)  # Courant appliqué
+
+dn/dt = alphan * (1-n) - betan * n : 1
+dm/dt = alpham * (1-m) - betam * m : 1
+dh/dt = alphah * (1-h) - betah * h : 1
+
+alphan = (0.01/mV) * 10*mV/exprel((-v+10*mV)/(10*mV))/ms : Hz
+betan = 0.125*exp(-v/(80*mV))/ms : Hz
+
+alpham = (0.1/mV) * 10*mV/exprel((-v+25*mV)/(10*mV))/ms : Hz
+betam = 4 * exp(-v/(18*mV))/ms : Hz
+
+alphah = 0.07 * exp(-v/(20*mV))/ms : Hz
+betah = 1/(exp((-v+30*mV) / (10*mV)) + 1)/ms : Hz
+
+gNa : siemens
+gK : siemens
+gl : siemens
+'''
+
+# Créer un groupe de neurones avec les équations définies
+HH = NeuronGroup(10, eqs, threshold='v>-20*mV', reset='v=0*mV', refractory=5*ms, method='rk4')
+#reset='v=El'
+# Initialisation des variables
+HH.v = El
+HH.h = 0.75
+HH.m = 0.15
+HH.n = 0.35
+HH.gNa = gNa0
+HH.gK = gK0
+HH.gl = gl0
+
+# Enregistrer les variables d'état et les spikes
+statemon = StateMonitor(HH, True, record=True)
+spikemon = SpikeMonitor(HH)
+
+# Simulation avec différents courants appliqués
+HH.I = 0.0*uA
+run(50*ms, report='text')
+HH.I = 60.0*uA
+run(50*ms, report='text')
+HH.I = 0.0*uA
+run(50*ms, report='text')
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 # Premier onglet :
     
 tab1 = QWidget() # cration d'un widget pour le premier onglet
@@ -74,6 +131,16 @@ tab1.setLayout(tab1.layout)
 #Premier widget :
 plot_widget = pg.PlotWidget()# Crée un widget de tracé pour l'oscilloscope
 tab1.layout.addWidget(plot_widget) # Ajoute le widget de tracé à l'onglet 1 (tab1)
+
+#HH :
+plot1_widget = pg.PlotWidget()
+tab1.layout.addWidget(plot1_widget)
+plot1_widget.plot(statemon.t/ms, statemon.v[0], pen='k')  # Affichage du résultat de la simulation HH
+plot1_widget.setLabel('left', 'Membrane potential (mV)')
+plot1_widget.setLabel('bottom', 'Time (ms)')
+plot1_widget.showGrid(x=True, y=True, alpha=0.2)
+#fin HH
+
 
 # Graphique du potentiel membranaire
 time, voltage, spikes = run_neuron_simulation() # Lance la simulation neuronale et récupère les données
@@ -100,6 +167,23 @@ scatter_widget.setLabel('bottom', 'Temps (ms)')
 
 if len(spikes) > 0:
     scatter_widget.plot(spikes, np.ones_like(spikes) * 1, pen=None, symbol='o', symbolSize=5, symbolBrush='r')# Trace les spikes
+
+#spikes HH :
+tab2 = QWidget()
+tab_widget.addTab(tab2, "Spikes")
+tab2.layout = QVBoxLayout()
+tab2.setLayout(tab2.layout)
+plot2_widget = pg.PlotWidget()
+tab2.layout.addWidget(plot2_widget)
+
+# Affichage des spikes
+plot2_widget.plot(spikemon.t/ms, np.array(spikemon.i), pen=None, symbol='o')
+
+# Ajout des labels pour le graphique des spikes
+plot2_widget.setLabel('left', 'Neuron index')
+plot2_widget.setLabel('bottom', 'Time (ms)')
+
+#fin spikes HH
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
